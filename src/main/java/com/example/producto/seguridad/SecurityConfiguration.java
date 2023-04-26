5f050c60326a999a4005b51b6508f0da0cc5c403
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2023-04-26 12:01:51 +0200

    Corrigiendo errores

--- src/main/java/com/example/producto/seguridad/SecurityConfiguration.java
+++ src/main/java/com/example/producto/seguridad/SecurityConfiguration.java
@@ -4,9 +4,13 @@ import com.example.producto.servicio.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+class SecurityConfiguration {
 
     @Autowired
     private UsuarioServicio usuarioServicio;
--- src/main/java/com/example/producto/seguridad/WebSecurityConfigurerAdapter.java
+++
@@ -1,4 +0,0 @@
-package com.example.producto.seguridad;
-
-public class WebSecurityConfigurerAdapter {
-}
