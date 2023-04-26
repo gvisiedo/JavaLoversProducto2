58225f46657cdebd5887e05f66049766fd387721
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2023-04-26 12:22:48 +0200

    Corrigiendo errores

--- src/main/java/com/example/producto/servicio/UsuarioServicioImpl.java
+++ src/main/java/com/example/producto/servicio/UsuarioServicioImpl.java
@@ -5,6 +5,12 @@ import com.example.producto.modelo.Rol;
 import com.example.producto.modelo.Usuario;
 import com.example.producto.repositorio.UsuarioRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -27,7 +33,7 @@ public class UsuarioServicioImpl implements UsuarioServicio {
        return usuarioRepositorio.save(usuario);
     }
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Usuario usuario = usuarioRepositorio.findByEmail(username);
         if (usuario == null){
             throw new UsernameNotFoundException("Usuario o password inválidos");
