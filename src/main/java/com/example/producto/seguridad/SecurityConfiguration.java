7e63e800018dba2353f959a876c14d4221353939
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2023-05-10 12:33:23 +0200

    Añadiendo dependencias

--- src/main/java/com/example/producto/seguridad/SecurityConfiguration.java
+++ src/main/java/com/example/producto/seguridad/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -32,7 +33,7 @@ class SecurityConfiguration {
         auth.authenticationProvider(authenticationProvider());
     }
     @Override
-    protected void configure(HttpcSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(
                 "/registro**", "/js/**",
                 "/css/**", "/img/**").permitAll().anyRequest().authenticated()
